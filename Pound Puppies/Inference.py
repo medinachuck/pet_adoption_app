aa978ac086f976cabe617358b1013b22c505a9f9
Sbrown404 <70172533+Sbrown404@users.noreply.github.com>
2021-03-13 17:38:06 -0800

    Add files via upload

---
+++ Pound Puppies/Inference.py
@@ -0,0 +1,8 @@
+def predict_single(img_file):
+    'function to take image and return prediction'
+    prediction = learn.predict(open_image(img_file))
+    probs_list = prediction[2].numpy()
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
